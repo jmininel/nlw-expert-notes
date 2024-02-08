b5658278cc7cf2ab78c459162fedb4b019080fc7
jmininel <ju.mininel@hotmail.com>
2024-02-08 18:47:26 -0300

    chore created setup with tailwind

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import logo from './assets/logo-nlw-expert.svg'
+
+export function App() {
+ 
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="NLW Expert" />
+
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500"
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <div className="rounded-md bg-slate-700 p-5 space-y-3">
+          <span className="text-sm font-medium text-slate-200">
+            Adicionar nota
+          </span>
+          <p className="text-sm leading-6 text-slate-400">
+            Grave uma nota em áudio que será convertida para texto
+            automaticamnte
+          </p>
+        </div>
+
+        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative ">
+          <span className="text-sm font-medium text-slate-300">há 2 dias</span>
+          <p className="text-sm leading-6 text-slate-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+            aut recusandae fuga, quod repellat nemo doloribus odio quis quidem
+            molestiae iste ipsa eveniet ratione? Non dicta quibusdam sint
+            numquam quia.
+          </p>
+          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+        </div>
+
+        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+          <span className="text-sm font-medium text-slate-300">há 4 dias</span>
+          <p className="text-sm leading-6 text-slate-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            sequi optio impedit perferendis accusantium! Expedita provident
+            impedit labore officia odit, soluta distinctio corporis in tempore
+            nulla! Obcaecati, asperiores. Nesciunt, repudiandae.
+          </p>
+          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
