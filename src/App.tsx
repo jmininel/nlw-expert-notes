33e133351a5ee8fc6436679d0e246c49182cef34
jmininel <ju.mininel@hotmail.com>
2024-02-08 20:14:03 -0300

    feat: created modal with radix dialog

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import NewNoteCard from './components/new-note-card';
 import NoteCard from './components/note-card';
 
 export function App() {
+
+  const note = {
+    date: new Date(),
+    content: 'hello',
+  }
  
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -20,10 +25,7 @@ export function App() {
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
-
-        <NoteCard />
-        <NoteCard />
-       
+        <NoteCard note={note} />
       </div>
     </div>
   );
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,15 +1,38 @@
-const NoteCard  = () => {
-  return (
-    <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 outline-none focus-visible:ring-2 focus-visible:ring-lime-400">
-      <span className="text-sm font-medium text-slate-300">há 2 dias</span>
-      <p className="text-sm leading-6 text-slate-400">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias aut
-        recusandae fuga, quod repellat nemo doloribus odio quis quidem molestiae
-        iste ipsa eveniet ratione? Non dicta quibusdam sint numquam quia.
-      </p>
-      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-    </button>
-  );
+import * as Dialog from "@radix-ui/react-dialog";
+import { formatDistanceToNow } from 'date-fns';
+import {ptB, ptBR } from 'date-fns/locale';
+
+interface NoteCardProps {
+  note: {
+    date: Date;
+    content: string;
+  };
 }
+
+const NoteCard = ({note}: NoteCardProps) => {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger className="flex flex-col rounded-md text-left bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 outline-none focus-visible:ring-2 focus-visible:ring-lime-400">
+        <span className="text-sm font-medium text-slate-300">
+          {note.date.toISOString()}
+        </span>
+        <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/50" />
+        <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] h-[60vh] w-full bg-slate-700 rounded-md flex flex-col outline-none">
+          <div className="flex flex-1 flex-col gap-3 p-5 ">
+            <span className="text-sm font-medium text-slate-300">
+              {formatDistanceToNow(note.date, {locale: ptBR, addSuffix: true})}
+            </span>
+            <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+          </div>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+};
  
 export default NoteCard;
