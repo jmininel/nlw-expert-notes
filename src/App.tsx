bf38b8b4b59dcd0926dcd8b04f1348f855882e36
jmininel <ju.mininel@hotmail.com>
2024-02-08 19:23:09 -0300

    feat: created components cardnote and newcardnote

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import logo from './assets/logo-nlw-expert.svg'
+import NewNoteCard from './components/new-note-card';
+import NoteCard from './components/note-card';
 
 export function App() {
  
@@ -17,37 +19,11 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <div className="rounded-md bg-slate-700 p-5 space-y-3">
-          <span className="text-sm font-medium text-slate-200">
-            Adicionar nota
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Grave uma nota em áudio que será convertida para texto
-            automaticamnte
-          </p>
-        </div>
+        <NewNoteCard />
 
-        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative ">
-          <span className="text-sm font-medium text-slate-300">há 2 dias</span>
-          <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-            aut recusandae fuga, quod repellat nemo doloribus odio quis quidem
-            molestiae iste ipsa eveniet ratione? Non dicta quibusdam sint
-            numquam quia.
-          </p>
-          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-        </div>
-
-        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
-          <span className="text-sm font-medium text-slate-300">há 4 dias</span>
-          <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
-            sequi optio impedit perferendis accusantium! Expedita provident
-            impedit labore officia odit, soluta distinctio corporis in tempore
-            nulla! Obcaecati, asperiores. Nesciunt, repudiandae.
-          </p>
-          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-        </div>
+        <NoteCard />
+        <NoteCard />
+       
       </div>
     </div>
   );
