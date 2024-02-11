d4042dceae4dd6e33a8b514c52e7adbfb1908f79
jmininel <ju.mininel@hotmail.com>
2024-02-11 17:34:25 -0300

    feat: created function to save data to localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,56 @@
+import { ChangeEvent, useState } from 'react';
 import logo from './assets/logo-nlw-expert.svg'
 import NewNoteCard from './components/new-note-card';
 import NoteCard from './components/note-card';
 
-export function App() {
+interface Note {
+  id: string
+  date: Date
+  content: string
+}
 
-  const note = {
-    date: new Date(),
-    content: 'hello',
+
+export function App() {
+  const [search, setSearch] = useState('')
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorage = localStorage.getItem('notes')
+
+    if (notesOnStorage) {
+      return JSON.parse(notesOnStorage)
+    }
+    return []
+  });
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    }
+    const notesArray = [newNote, ...notes];
+
+    setNotes(notesArray);
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
   }
- 
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value;
+
+    setSearch(query);
+  }
+
+  const filteredNotes = search !== ''
+    ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+    : notes
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="NLW Expert" />
 
       <form className="w-full">
         <input
+          onChange={handleSearch}
           type="text"
           placeholder="Busque em suas notas..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500"
@@ -24,8 +60,11 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <NewNoteCard />
-        <NoteCard note={note} />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+
+        {filteredNotes.map((note) => {
+          return <NoteCard key={note.id} note={note} />;
+        })}
       </div>
     </div>
   );
--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -4,29 +4,35 @@ import { X } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { toast } from 'sonner'
 
-const NewNoteCard = () => {
-  const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true);
-  const [content, setContent] = useState('')
+interface NewNoteCardProps {
+  onNoteCreated: (content: string) => void
+}
 
-  function handleStartEditor()  {
-   setShouldShowOnboarding(false)
+const NewNoteCard = ({ onNoteCreated }: NewNoteCardProps) => {
+  const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true);
+  const [content, setContent] = useState("");
+
+  function handleStartEditor() {
+    setShouldShowOnboarding(false);
   }
 
-  function handleContentChange(event: ChangeEvent<HTMLTextAreaElement> ) {
-    setContent(event.target.value)
-    
-    if (event.target.value === '') {
-      setShouldShowOnboarding(true)
+  function handleContentChange(event: ChangeEvent<HTMLTextAreaElement>) {
+    setContent(event.target.value);
+
+    if (event.target.value === "") {
+      setShouldShowOnboarding(true);
     }
   }
 
   function handleSaveNote(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-    console.log(content);
+    event.preventDefault();
 
-    toast.success('Nota criada com sucesso')
+    onNoteCreated(content)
+    setContent('')
+    setShouldShowOnboarding(true)
+    toast.success("Nota criada com sucesso");
   }
-  
+
   return (
     <Dialog.Root>
       <Dialog.Trigger className="flex flex-col text-left rounded-md bg-slate-700 p-5 gap-3 hover:ring-2 hover:ring-slate-600 outline-none focus-visible:ring-2 focus-visible:ring-lime-400">
@@ -45,30 +51,44 @@ const NewNoteCard = () => {
             <X className="size-5" />
           </Dialog.Close>
 
-        <form onSubmit={handleSaveNote} className="flex-1 flex flex-col">
-          <div className="flex flex-1 flex-col gap-3 p-5 ">
-            <span className="text-sm font-medium text-slate-300">
-              Adicionar nota
-            </span>
+          <form onSubmit={handleSaveNote} className="flex-1 flex flex-col">
+            <div className="flex flex-1 flex-col gap-3 p-5 ">
+              <span className="text-sm font-medium text-slate-300">
+                Adicionar nota
+              </span>
 
-            {shouldShowOnboarding ? (
-              <p className="text-sm leading-6 text-slate-400">
-                Comece <button className="font-medium text-lime-400 hover:underline">gravando uma nota</button> em áudio ou se preferir <button onClick={handleStartEditor} className="font-medium text-lime-400 hover:underline"> utilize apenas texto</button>
-              </p>
-            ) : (
-                < textarea autoFocus className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
-                 onChange={handleContentChange}
+              {shouldShowOnboarding ? (
+                <p className="text-sm leading-6 text-slate-400">
+                  Comece{" "}
+                  <button className="font-medium text-lime-400 hover:underline">
+                    gravando uma nota
+                  </button>{" "}
+                  em áudio ou se preferir{" "}
+                  <button
+                    onClick={handleStartEditor}
+                    className="font-medium text-lime-400 hover:underline"
+                  >
+                    {" "}
+                    utilize apenas texto
+                  </button>
+                </p>
+              ) : (
+                <textarea
+                  autoFocus
+                  className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
+                    onChange={handleContentChange}
+                    value={content}
                 />
-            )}
-          </div>
+              )}
+            </div>
 
-          <button
-            type="submit"
-            className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500 "
-          >
-            Salvar nota
-          </button>
-        </form>  
+            <button
+              type="submit"
+              className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500 "
+            >
+              Salvar nota
+            </button>
+          </form>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
